9be240609cf6d3dc584ef92aa2287926eb53c9d2
Stephen Martin <stephensrmmartin@gmail.com>
2020-05-07 18:59:26 -0700

    Added %IfNull% operator for defaults handling.

---
+++ R/utility.R
@@ -0,0 +1,12 @@
+##' @title Operator for testing NULL and returning expr if NULL
+##' @param object Object to test for NULL
+##' @param expr Expression to evaluate and return if object is NULL
+##' @return object if object is non-NULL, expression output if object is NULL.
+##' @author Stephen R. Martin
+`%IfNull%` <- function(object, expr) {
+    if(is.null(object)) {
+        return(eval(expr))
+    } else {
+        return(object)
+    }
+}
