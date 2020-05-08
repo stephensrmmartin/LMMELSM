54e44d2171d760bb76e1d25b8a3c41ae35b4e915
Stephen Martin <stephensrmmartin@gmail.com>
2020-05-07 19:00:37 -0700

    Roxygen

--- R/utility.R
+++ R/utility.R
@@ -3,6 +3,7 @@
 ##' @param expr Expression to evaluate and return if object is NULL
 ##' @return object if object is non-NULL, expression output if object is NULL.
 ##' @author Stephen R. Martin
+##' @keywords internal
 `%IfNull%` <- function(object, expr) {
     if(is.null(object)) {
         return(eval(expr))
