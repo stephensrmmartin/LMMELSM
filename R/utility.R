7bbf6c43f5c154986e234212b92a86256ac00709
Stephen Martin <stephensrmmartin@gmail.com>
2020-05-18 23:32:42 -0700

    FIX: nlist incorrect if all args unnamed

--- R/utility.R
+++ R/utility.R
@@ -22,11 +22,19 @@ nlist <- function(...) {
     out <- list(...)
 
     not_named <- is.null(names(out))
-    is_named <- (!not_named) & nzchar(names(out))
+    is_named <- if(not_named) {
+                    FALSE
+                } else {
+                    nzchar(names(out))
+                }
 
     args <- as.character(mc)[-1] # Not the fn name.
 
-    names(out)[!is_named] <- args[!is_named]
+    if(not_named) {
+        names(out) <- args
+    } else {
+        names(out)[!is_named] <- args[!is_named]
+    }
 
     return(out)
 }
