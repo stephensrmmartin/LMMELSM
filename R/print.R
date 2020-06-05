19d22ae0053b9f6afb08e6c22222d97717835d11
Stephen Martin <stephensrmmartin@gmail.com>
2020-06-04 18:38:59 -0700

    Added print.lmmelsm

--- R/print.R
+++ R/print.R
@@ -22,8 +22,60 @@
     }
 }
 
+.tab <- function() {
+    cat("\t")
+}
+##' Print method for lmmelsm objects.
+##' @title 
+##' @param x lmmelsm object.
+##' @param ... Not used.
+##' @return NULL.
+##' @author Stephen R. Martin
+##' @export
 print.lmmelsm <- function(x, ...) {
-    
+    .sep()
+
+    cat("Estimated on:")
+    cat(x$fit@date)
+    .newline()
+    cat("Elapsed time (seconds): ")
+    cat(.get_elapsed_time(x))
+
+    .sep()
+
+    cat("Model specification:")
+    .newline(2)
+
+    cat("Measurement model")
+    .newline()
+    lapply(x$meta$indicator_spec$mlist, function(f) {
+        cat(.print.formula(f))
+        .newline()
+    })
+
+    if(x$meta$pred_spec$P > 0) {
+        .newline()
+        cat("Location model:")
+        .newline()
+        cat(.print.formula(x$meta$pred_spec$plist$location))
+        .newline()
+    }
+
+    if(x$meta$pred_spec$Q > 0) {
+        .newline()
+        cat("Scale model")
+        .newline()
+        cat(.print.formula(x$meta$pred_spec$plist$scale))
+        .newline()
+    }
+
+    if(x$meta$pred_spec$R > 0) {
+        .newline()
+        cat("Between-group scale model")
+        .newline()
+        cat(.print.formula(x$meta$pred_spec$plist$between))
+        .newline()
+    }
 }
 
 summary.lmmelsm <- function(object, ...) {
@@ -32,4 +84,16 @@ summary.lmmelsm <- function(object, ...) {
 
 print.summary.lmmelsm <- function(x, ...) {
     
+}
+
+.print.formula <- function(f) {
+    string <- Reduce(paste0, deparse(f))
+    return(string)
+}
+
+.get_elapsed_time <- function(sOut) {
+    times <- rstan::get_elapsed_time(sOut$fit)
+    out <- max(rowSums(times))
+
+    return(out)
 }
