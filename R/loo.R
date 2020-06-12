0cf193f02a047aa719227fef5163285300bb8768
Stephen Martin <stephensrmmartin@gmail.com>
2020-06-11 19:31:35 -0700

    Loo skeleton

---
+++ R/loo.R
@@ -0,0 +1,16 @@
+
+##' @title loo method for LMMELSM objects.
+##' @param x lmmelsm object.
+##' @param ... Not used.
+##' @return loo object.
+##' @author Stephen R. Martin
+##' @importFrom loo loo
+##' @export loo
+##' @export
+loo.lmmelsm <- function(x, ...) {
+    
+}
+
+.log_lik <- function(x) {
+    
+}
