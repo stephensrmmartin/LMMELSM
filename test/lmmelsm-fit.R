4b3048a7fcceed4678b28d1e6e5b2392c61edddc
Stephen R. Martin <stephenSRMMartin@gmail.com>
2021-02-17 19:24:07 -0800

    Test fns.

--- test/lmmelsm-fit.R
+++ test/lmmelsm-fit.R
@@ -22,3 +22,5 @@ d <- LMMELSM:::simulate_lmmelsm(
 
 sOut <- lmmelsm(list(factor1 ~ obs_1 + obs_2 + obs_3 + obs_4 + obs_5, location ~ loc_1 + loc_2 | loc_1 + loc_2, scale ~ sca_1 + sca_2 | sca_1 + sca_2), subject, d$df, iter = 500)
 
+d$params$mu_logsd_betas_re[,1:2]
+ranef(sOut)$location[,"Mean", drop = FALSE]
